33ef434ec452db7e59f03148a826cf8af8f26e13
lzlocki <lzlocki@poczta.fm>
2023-09-07 19:23:53 +0200

    Add first test of time resolve class

--- 25_ETL_ProductionLine_MicroStopsReport/main.py
+++ 25_ETL_ProductionLine_MicroStopsReport/main.py
@@ -7,6 +7,7 @@
 from pathlib import Path
 import data_extract as de
 import microstops_report as ms
+import time_resolver as tr
 
 SOURCE_PATH = Path("./src/")
 OUTPUT_PATH = Path("./out/")
@@ -31,15 +32,19 @@ ms_report.print_dataset(ms_report._csv_dataset, 10)
 # * Micro stops time resolving *
 # Steps ToDo: Create dictionary to cover all micro stops times (like 1h -> 60 [minutes], like 1h 30minut -> 90 [minutes] )
 # Create class TimeResolver with all algorithms to transfer each description into time in [minutes]
-print("Printing 10 dataset from raw csv and only column with waste time description")
-ms_report.print_given_column_in_dataset(ms_report._csv_dataset, 5, 0)
+print("Printing 5 dataset from raw csv and only column with waste time description")
+ms_report.print_given_column_in_dataset(ms_report._csv_dataset, 5, 5)
+
+# resolving time from string - TESTS ! 
+time_resolver = tr.TimeResolver()
+string_array = time_resolver._resolve_this_string_to_separate_words_and_numbers("1h 24min")
+print(string_array)
 
 # Steps ToDo: Create method pointing that script is not able to resolve micro stop given b operator . This operation will be done manualy and description will be added to dictionary manualy
 # Steps TodO: Save data to output csv file
 # * Micro stops root cause standardization *
 # Steps ToDo: steps need to be staed here
 # Steps ToDo: steps need to be stated here
-
 
 if __name__ == '__main__':
     main() 
