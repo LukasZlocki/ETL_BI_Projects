7bb6abaaa8cfcf0e2b3c7deae83f41470e66735d
lzlocki <lzlocki@poczta.fm>
2023-09-21 18:19:35 +0200

    Code cleanup

--- 25_ETL_ProductionLine_MicroStopsReport/main.py
+++ 25_ETL_ProductionLine_MicroStopsReport/main.py
@@ -17,6 +17,7 @@ RAW_CSV_DATA_MICROSTOPS = ("raw_micro_stops.csv")
 def main():
     '''
     '''
+
 # Step: Load data from excel
 data_extractor = de.DataExtractor ()
 data_extractor.extract_micro_stops_data_from_excel_sheet(SOURCE_PATH, SOURCE_FILE)
@@ -33,54 +34,9 @@ ms_report.print_dataset(ms_report._csv_dataset, 10)
 # Steps ToDo: Create dictionary to cover all micro stops times (like 1h -> 60 [minutes], like 1h 30minut -> 90 [minutes] )
 # Create class TimeResolver with all algorithms to transfer each description into time in [minutes]
 print("Printing 10 dataset from raw csv and only column with waste time description")
-ms_report.print_given_column_in_dataset(ms_report._csv_dataset, 5, 5)
-ms_report.resolve_time_description_transform_to_time_in_minutes() # << -- !!
-
-# DELETE this
-'''
-#Write a test methods to cover testing of the code and different scenerios
-# resolving time from string - TESTS ! 
-time_resolver = tr.TimeResolver()
-string_array = time_resolver._extracting_words_and_numbers_from_string("1h 24 min")
-print(string_array)
-string_array = time_resolver._extracting_words_and_numbers_from_string("10 m")
-print(string_array)
-string_array = time_resolver._extracting_words_and_numbers_from_string("26.")
-print(string_array)
-string_array = time_resolver._extracting_words_and_numbers_from_string("10m")
-print(string_array)
-string_array = time_resolver._extracting_words_and_numbers_from_string("")
-print(string_array)
-string_array = time_resolver._extracting_words_and_numbers_from_string(" ")
-print(string_array)
-string_array = time_resolver._extracting_words_and_numbers_from_string("3 godziny")
-print(string_array)
-'''
-
-
-'''
-# Test 1: "1 godz. 30 m.""
-string_array = time_resolver._extracting_words_and_numbers_from_string("1 godz. 30 m.")
-print(string_array)
-print("Testing retriving multipilcator and number. Test Variant: 1 godz. 30 m. ")
-resolved_time = time_resolver._extracting_time_from_array_of_words_and_numbers(string_array)
-for element in resolved_time:
-    print(element)
-time = time_resolver._resolve_time_from_final_array(resolved_time)
-print(time)
-
-# Test 2: "5""   !!!! FIX THIS IN  CODE ! 
-string_array = time_resolver._extracting_words_and_numbers_from_string("5")
-print(string_array)
-print("Testing retriving multipilcator and number. Test Variant: 5")
-resolved_time = time_resolver._extracting_time_from_array_of_words_and_numbers(string_array)
-for element in resolved_time:
-    print(element)
-time = time_resolver._resolve_time_from_final_array(resolved_time)
-print(time)
-#'''
-
-
+ms_report.print_given_column_in_dataset(ms_report._csv_dataset, 5, 58)
+print("Changing time")
+ms_report.resolve_time_description_and_transformstrings_to_time_in_minutes(5) # << -- column nb 5 consist described time
 
 
 # Steps ToDo: Create method pointing that script is not able to resolve micro stop given b operator . This operation will be done manualy and description will be added to dictionary manualy
